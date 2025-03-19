dcba979b446bee32a1aeb09fe87b36c20ba694ca
Park Jongwoon <nortiz1349@gmail.com>
2025-03-19 16:33:47 +0900

    maneuvers to Optional

--- Sources/ValhallaModels/Models/RouteLeg.swift
+++ Sources/ValhallaModels/Models/RouteLeg.swift
@@ -12,12 +12,12 @@ import Foundation
 
 public struct RouteLeg: Codable, Hashable {
     static let maneuversRule = ArrayRule(minItems: 1, maxItems: nil, uniqueItems: false)
-    public var maneuvers: [RouteManeuver]
+    public var maneuvers: [RouteManeuver]?
     /** An encoded polyline (https://developers.google.com/maps/documentation/utilities/polylinealgorithm) with 6 digits of decimal precision. */
     public var shape: String
     public var summary: RouteSummary
 
-    public init(maneuvers: [RouteManeuver], shape: String, summary: RouteSummary) {
+    public init(maneuvers: [RouteManeuver]? = nil, shape: String, summary: RouteSummary) {
         self.maneuvers = maneuvers
         self.shape = shape
         self.summary = summary
@@ -33,7 +33,7 @@ public struct RouteLeg: Codable, Hashable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(maneuvers, forKey: .maneuvers)
+        try container.encodeIfPresent(maneuvers, forKey: .maneuvers)
         try container.encode(shape, forKey: .shape)
         try container.encode(summary, forKey: .summary)
     }
